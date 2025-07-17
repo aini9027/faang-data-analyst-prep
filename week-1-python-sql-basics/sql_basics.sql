83232d2b7e2d8be1b43945c175ac8cf00395f1e5
aini9027 <32837714+aini9027@users.noreply.github.com>
2025-07-17 16:02:28 +0530

    Day 1: Added Python basics notebook and SQL queries

--- week-1-python-sql-basics/sql_basics.sql
+++ week-1-python-sql-basics/sql_basics.sql
@@ -1,1 +1,40 @@
+Day 1 SQL
+
+-- Create Table
+CREATE TABLE customers (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+  city TEXT
+);
+
+-- Insert Records
+INSERT INTO customers (id, name, age, city) VALUES
+(1, 'Alice', 30, 'New York'),
+(2, 'Bob', 22, 'Los Angeles'),
+(3, 'Charlie', 35, 'Chicago'),
+(4, 'Diana', 28, 'New York'),
+(5, 'Ethan', 42, 'Seattle');
+
+
+-- Select all columns
+SELECT * FROM customers;
+
+-- Select specific column
+SELECT name, city FROM customers;
+
+-- Filter by city
+SELECT * FROM customers WHERE city = 'New York';
+
+-- Filter by age
+SELECT * FROM customers WHERE age > 30;
+
+-- Order by age (descending)
+SELECT * FROM customers ORDER BY age DESC;
+
+-- Limit to top 2
+SELECT * FROM customers LIMIT 2;
+
+-- Use alias
+SELECT name AS customer_name, age FROM customers;
 
