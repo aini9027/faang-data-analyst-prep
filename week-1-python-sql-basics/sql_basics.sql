d315416e032f39b97d5791c162ceb024a739dbde
aini9027 <32837714+aini9027@users.noreply.github.com>
2025-07-23 23:17:36 +0530

    Update sql_basics.sql

--- week-1-python-sql-basics/sql_basics.sql
+++ week-1-python-sql-basics/sql_basics.sql
@@ -117,7 +117,57 @@ Find all customers who never placed an order.
 
 We need to find customers present in `Customers` but not in `Orders`.
 
----
 
+  Person
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| email       | varchar |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains an email. The emails will not contain uppercase letters.
+Select name as Customers
+from Customers 
+where id NOT  IN (select customerId from Orders);
+  
 ### **Key SQL Concepts Learned**
 - **LEFT**
+ 
+
+Write a solution to report all the duplicate emails. Note that it's guaranteed that the email field is not NULL.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Person table:
++----+---------+
+| id | email   |
++----+---------+
+| 1  | a@b.com |
+| 2  | c@d.com |
+| 3  | a@b.com |
++----+---------+
+Output: 
++---------+
+| Email   |
++---------+
+| a@b.com |
++---------+
+Explanation: a@b.com is repeated two times.
+
+
+  SELECT email
+FROM Person
+GROUP BY email
+HAVING COUNT(*) > 1;
+
+---
+
